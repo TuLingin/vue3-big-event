6ba4a4b9805a6eda0243dbda4a507a485f4a1720
xiaomin <xiaomin@github.cn>
2023-09-29 16:11:13 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from './stores'
+import router from '@/router'
+// import { useRouter } from 'vue-router'
+// const router = useRouter()
+// const route = useRoute()
+const goList = () => {
+  router.push('list')
+}
+const userStore = useUserStore()
+</script>
 
 <template>
-  <div>我是App</div>
+  <div>
+    <hr />
+    <router-view></router-view>
+    <hr />
+    我是App
+    <el-button type="primary" @click="goList">跳转</el-button>
+    <br />
+    <br />
+    <br />
+    {{ userStore.token }}
+    <el-button
+      type="primary"
+      @click="userStore.setToken('sdsddsdsdsdsdssdoids')"
+      >登录</el-button
+    >
+    <el-button type="primary" @click="userStore.removeToken">退出</el-button>
+  </div>
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,36 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/login', component: () => import('@/views/login/LoginPage.vue') },
+    {
+      path: '/',
+      component: () => import('@/views/layout/LayoutContainer.vue'),
+      redirect: '/article/manage',
+      children: [
+        {
+          path: '/article/manage',
+          component: () => import('@/views/article/ArticleManager.vue')
+        },
+        {
+          path: '/article/chanel',
+          component: () => import('@/views/article/ArticleChanel.vue')
+        },
+        {
+          path: '/user/avatar',
+          component: () => import('@/views/user/UserAvatar.vue')
+        },
+        {
+          path: '/user/password',
+          component: () => import('@/views/user/UserPassword.vue')
+        },
+        {
+          path: '/user/profile',
+          component: () => import('@/views/user/UserProfile.vue')
+        }
+      ]
+    }
+  ]
 })
 
 export default router
